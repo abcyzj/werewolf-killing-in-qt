572a4d789a84ea01176fc954fe456a6ea0fdbf57
王琛 <wangchen@wangchendeMacBook-Air.local>
2017-06-14 14:16:30 +0800

    add annotations

--- server/character.cpp
+++ server/character.cpp
@@ -1,41 +1,50 @@
+/*******************************************
+ 名称：character.cpp
+ 作者:王琛 计65 2016011360
+ 时间：2017/05/15
+ 内容：角色类中各种函数的实现
+ 版权：自行完成的程序，没有使用其余的源代码
+ *******************************************/
+
 #include "character.h"
 using namespace Werewolf;
 
-void Character :: set_police()
+void Character :: set_police()  //将该角色设置为警长
 {
   police = 1;
 }
 
-void Character :: set_dead()
+void Character :: set_dead()    //将该角色设置为死亡
 {
   dead = 1;
 }
 
-void Character :: guardfunc()
+void Character :: guardfunc()   //将该角色置于被守卫状态
 {
   _guard = 1;
 }
 
-void Character :: set_alive()
+void Character :: set_alive()   //将该角色救活
 {
   dead = 0;
 }
 
-bool Character :: is_police()
+bool Character :: is_police()   //判断该角色是否为警长
 {
   return police;
 }
 
-bool Character :: is_dead()
+bool Character :: is_dead()     //判断该角色是否已经死亡
 {
   return dead;
 }
 
-bool Character :: is_guarded()
+bool Character :: is_guarded()  //判断该角色是否被守卫
 {
   return _guard;
 }
 
+//以下是返回各个角色的类型
 int Wolf :: type()
 {
   return wolf;
@@ -65,30 +74,31 @@ int Guard :: type()
 {
   return guard;
 }
+//到此为止
 
-Witch :: Witch() : poison(1), antidote(1) {}    //一瓶毒药，一瓶解药
+Witch :: Witch() : poison(1), antidote(1) {}//女巫开始一瓶毒药，一瓶解药
 
 void Witch :: use_poison()  //使用毒药
 {
   poison = 0;
 }
 
-void Witch :: use_antidote()    //使用解药
+void Witch :: use_antidote() //使用解药
 {
   antidote = 0;
 }
 
-bool Witch :: have_poison()
+bool Witch :: have_poison()  //判断是否剩下毒药
 {
   return poison;
 }
 
-bool Witch :: have_antidote()
+bool Witch :: have_antidote()//判断是否剩下解药
 {
   return antidote;
 }
 
-void Guard :: who_i_guard(int ch)
+void Guard :: who_i_guard(int ch) //被守卫的人编号，守卫输入时传入
 {
   per_guard = ch;
 }
--- server/character.h
+++ server/character.h
@@ -1,27 +1,36 @@
+//Name : character.h
+//Author : 王琛
+//Date : 2017-05-15
+//Description : 声明了所有角色的基类和子类
+//Copyright : 自行完成
+
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
-//村民 狼人 女巫 守卫 猎人 预言家
 namespace Werewolf
 {
+  //使用enum枚举类定义不同角色的编号
+  //从1-6分别为狼人，村民，猎人，女巫，预言家，守卫（编号顺序人为规定，不同顺序不影响程序执行）
   enum ch_type {wolf = 1, villager, hunter, witch, seer, guard};
     
+  //Character类是所有角色的基类（抽象类），声明了所有角色共有的信息和操作
   class Character
   {
-    bool police = 0;
-    bool dead = 0;
-    bool _guard = 0;
+    bool police = 0;    //是否为警长
+    bool dead = 0;      //是否死亡
+    bool _guard = 0;    //是否被守卫
   public:
-    virtual int type() = 0; //返回一个int值代表角色类型
+    virtual int type() = 0; //纯虚函数，返回一个int值代表角色类型
     bool is_dead();     //是否死亡
     bool is_police();	//是否为警长
     bool is_guarded();  //处于被守护状态
-    void guardfunc();   //守卫
+    void guardfunc();   //被守卫
     void set_alive();   //复活（女巫解药）
     void set_dead();    //被杀死
     void set_police();  //设为警长
   };
-    
+
+  //下面六个类为6种角色，除了女巫守卫其他只需要重写type虚函数
   class Wolf : public Character
   {
   public:
@@ -42,14 +51,14 @@ namespace Werewolf
     
   class Witch : public Character
   {
-    int poison,antidote;
+    int poison,antidote;    //毒药解药的数量
   public:
     Witch();
     int type();
-    void use_poison();
-    void use_antidote();
-    bool have_poison();
-    bool have_antidote();
+    void use_poison();      //使用毒药
+    void use_antidote();    //使用解药
+    bool have_poison();     //有毒药
+    bool have_antidote();   //有解药
   };
     
   class Seer: public Character
@@ -62,9 +71,9 @@ namespace Werewolf
   {
     int per_guard = -1;
   public:
-    void who_i_guard(int ch);
+    void who_i_guard(int ch);   //守卫的人
     int type();
-    int last();  //  上一晚守卫的人
+    int last();                 //上一晚守卫的人
   };
 }
 
